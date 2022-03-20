b51c9c518f0ffe0172d81710282edbf2d32a353f
Nick Garza <thenickgarza14@gmail.com>
2022-03-20 16:07:42 -0500

    added the date format to the utils folder

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const UserSchema = new Schema(
             required: true,
             trim: true,
             unique: true,
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please use a valid email address']
         },
         thoughts: [
             {
